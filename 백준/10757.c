bb463e8feb3fba131764d6e0de472af3a0e29a7a
clcok <ekfrha24@naver.com>
2024-03-19 23:18:11 +0900

    2024.03.19

---
+++ 백준/10757.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void main(){
+  uint64_t n1, n2;
+  scanf("%d %d", &n1, &n2);
+  printf("%d", n1 + n2);
+}
---
+++ 백준/5355.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+  int n;
+  scanf("%d", &n);
+
+  float input;
+  char operand;
+
+  for (int i=0;i<n;i++){
+    scanf("%lf", &input);
+    //
+  }
+}
